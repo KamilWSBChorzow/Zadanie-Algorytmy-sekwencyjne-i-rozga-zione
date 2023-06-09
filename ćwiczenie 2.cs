71fa05679d30ebb1507a784e271aa9ccf93b13cd
KamilWSBChorzow <128227099+KamilWSBChorzow@users.noreply.github.com>
2023-06-09 23:28:31 +0200

    Add files via upload

---
+++ ćwiczenie 2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class MyProgram
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Podaj P");
+        double p;
+        
+        p = inputValue();
+        double r;
+        
+        r = Math.Sqrt(p / Math.PI);
+        Console.WriteLine(r);
+        int o;
+        
+        o = (int) (2 * Math.PI * r);
+        Console.WriteLine("Pole koła wynosi:" + o);
+    }
+    
+    // .NET can only read single characters or entire lines from the
+    // console. The following function safely reads a double value.
+    private static double inputValue()
+    {
+        double result;
+        while (!double.TryParse(Console.ReadLine(), out result));
+        return result;
+    }
+}
